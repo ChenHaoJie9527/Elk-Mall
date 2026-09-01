f6b7fe31a9df0ce631db474a2d457cb66c3f7643
ChenHaoJie9527 <a18520145493@163.com>
2026-09-01 23:06:48 +0800

    feat: 在主入口目录读取根目录config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,17 @@
-package server
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ChenHaoJie9527/Elk-Mall/internal/config"
+)
+
+func main() {
+	cfg, err := config.Load("config.yaml")
+	if err != nil {
+		log.Fatalf("加载配置文件失败: %v", err)
+	}
+
+	fmt.Printf("配置文件加载成功: %+v\n", cfg)
+}
