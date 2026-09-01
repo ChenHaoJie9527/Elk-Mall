8a40b0aaf862e14a05cf0402ca283ac689b2e71b
ChenHaoJie9527 <a18520145493@163.com>
2026-09-01 22:11:34 +0800

    config: 构建项目应用配置读取文件config.go

---
+++ internal/config/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+// Config 配置
+type Config struct {
+	App    AppConfig
+	Server AppServer
+}
+
+// AppConfig 应用配置
+type AppConfig struct {
+	Name string
+}
+
+// AppServer 应用服务器配置
+type AppServer struct {
+	Port string
+}
+
+// Load 加载配置文件
+func Load(path string) (*Config, error) {
+	// 初始化 viper
+	vi := viper.New()
+	// 设置配置文件的路径
+	vi.SetConfigFile(path)
+	// 设置默认值，如果配置文件中没有设置，则使用默认值
+	vi.SetDefault("server.port", "8080")
+	// 将 server.port 转换为 SERVER_PORT，用于环境变量
+	vi.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	// 自动加载环境变量
+	vi.AutomaticEnv()
+
+	// 读取配置文件
+	if err := vi.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("读取配置文件失败: %v", err)
+	}
+
+	var cfg Config
+	// 将配置文件中的配置解析到 cfg 中
+	if err := vi.Unmarshal(&cfg); err != nil {
+		return nil, fmt.Errorf("解析配置文件失败: %v", err)
+	}
+
+	return &cfg, nil
+}
