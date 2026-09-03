b9173064788c58883e2ea0d66df83e6446493399
ChenHaoJie9527 <a18520145493@163.com>
2026-09-03 22:09:25 +0800

    config: compose 设置 redis容器

--- internal/controller/health.go
+++ internal/controller/health.go
@@ -14,10 +14,11 @@ type Health struct {
 
 // Ping 健康检查接口
 func (h *Health) Ping(c *echo.Context) error {
+	// 把请求上下文传递给 MySQL 连接，确保在请求上下文被取消时，MySQL 连接也会被关闭
 	if err := h.MySQL.PingContext(c.Request().Context()); err != nil {
 		return err
 	}
-
+	// 返回成功响应
 	return c.JSON(http.StatusOK, response.Success(map[string]string{
 		"mysql": "ok",
 	}))
