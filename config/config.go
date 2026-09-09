7aa015a6542323283400217ba0bab7560bd60545
ChenHaoJie9527 <a18520145493@163.com>
2026-09-09 20:39:14 +0800

    feat: 创建包内init初始化函数

--- config/config.go
+++ config/config.go
@@ -1,15 +1,19 @@
 package config
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 const (
-	ServerName     = "Mall"
+	ServerName     = "mall"
 	FullServerName = "elk.mall"
 )
 
 var (
 	etcd            = fmt.Sprintf("/configs/%s/system", FullServerName)
-	etcdKey         string // etcd 配置的 key
+	etcdAddr        string // etcd 地址
 	localConfigPath string // 本地配置配置文件路径
 	GlobalConfig    Config // 全局配置
 )
@@ -40,10 +44,25 @@ type MySQL struct {
 	MaxIdle  int    `yaml:"max_idle"`
 }
 
+// GetDSN 获取 MySQL 的 DSN -
+// 格式：user:password@tcp(host:port)/database?charset=charset&parseTime=true&loc=Local
+func (sql *MySQL) GetDSN() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=true&loc=Local",
+		sql.User, sql.Password, sql.Host, sql.Port, sql.Database, sql.Charset)
+}
+
 type Redis struct {
 	Addr    string `yaml:"addr"`
 	PWD     string `yaml:"password"`
 	DBIndex int    `yaml:"db_index"`
 	MaxIdle int    `yaml:"max_idle"`
 	MaxOpen int    `yaml:"max_open"`
+}
+
+// init 初始化配置
+func init() {
+	// 解析命令行参数 格式：-config=config.yaml
+	flag.StringVar(&localConfigPath, "config", ServerName+"_config.yaml", "default config path")
+	// 解析命令行参数 格式：-r=127.0.0.1:2379
+	flag.StringVar(&etcdAddr, "r", os.Getenv("ETCD_ADDR"), "default etcd address")
 }
